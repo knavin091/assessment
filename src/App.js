d0fb3ed304a70ffacf802f06c0f3fd3eb2b292a9
knavin091 <58015353+knavin091@users.noreply.github.com>
2021-11-15 11:23:41 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//import Register from "./Components/Auth/Customer/Register";
+
+import Login from "./Components/Auth/Customer/Login";
+import "./Styles/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Register /> */}
+      <Login />
     </div>
   );
 }
